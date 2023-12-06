0953354f561a36ec943ab363a955e00d249d2674
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2023-12-06 10:32:28 -0300

    Sinalizando a validação

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 Ela é formada por 4 pontos de controle, aonde você pode modificar a curva, pode adicionar a duração também, quando clica em gol, você consegue ver a animação como ira ficar, você pode personalizar como começa e termina
 
 [Cubic-bezier](https://cubic-bezier.com/#.17,.67,.83,.67) 
+
+
+Existem várias ferramentas online disponíveis para ajudar a criar e visualizar curvas personalizadas cubic-bezier, como o site cubic-bezier.com, que permite criar e visualizar curvas personalizadas cubic-bezier.
+
+A [Documentação oficial do MDN](https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#using_the_cubic-bezier_function) do MDN também traz informações interessantes sobre essa ferramenta.
+
+
+E o [easings net](https://easings.net/) fornece uma galeria de curvas de animação pré-definidas e permite que você crie suas próprias curvas personalizadas.
--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger, filterTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import { checkButtonTrigger, filterTrigger, formButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
@@ -14,13 +14,15 @@ import { checkButtonTrigger, filterTrigger, highlightedStateTrigger, shownStateT
     highlightedStateTrigger,
     shownStateTrigger,
     checkButtonTrigger,
-    filterTrigger
+    filterTrigger,
+    formButtonTrigger
   ]
 })
 
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
-  formAberto: boolean = false;
+  // true, ja fique com o furmulario aberto
+  formAberto: boolean = true;
   categoria: string = '';
   validado: boolean = false;
   indexTarefa: number = -1;
@@ -33,8 +35,8 @@ export class ListaTarefasComponent implements OnInit {
     id: [0],
     descricao: ['', Validators.required],
     statusFinalizado: [false, Validators.required],
-    categoria: ['', Validators.required],
-    prioridade: ['', Validators.required],
+    categoria: ['Casa', Validators.required],
+    prioridade: ['Alta', Validators.required],
   });
 
   constructor(
